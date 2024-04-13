8b5ac5dccda8865c77881da0afb2afed63f89022
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-13 02:33:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+
+
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const app = express();
+
+// HTTP server
+const httpServer = http.createServer(app);
+const HTTP_PORT = 80;
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`HTTP Server is running on port ${HTTP_PORT}`);
+});
+
+// HTTPS server
+const privateKey = fs.readFileSync('/Users/mots/Desktop/Part 3 API/private.key', 'utf8');
+const certificate = fs.readFileSync('/Users/mots/Desktop/Part 3 API/certificate.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+const HTTPS_PORT = 443;
+
+httpsServer.listen(HTTPS_PORT, () => {
+    console.log(`HTTPS Server is running on port ${HTTPS_PORT}`);
+});
+
+
+
+
+
+
+// Define your Express routes here
+app.get('/users', (req, res) => {
+    res.send("GET request on users resource");
+});
+
+
+
+
+
+
+
+
+
