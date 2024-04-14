3a1df47440943742acc392ea23edecbbece54c59
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-14 23:57:47 +0100

    removed https server setup
    was not working correctly

--- index.js
+++ index.js
@@ -14,19 +14,6 @@ httpServer.listen(HTTP_PORT, () => {
     console.log(`HTTP Server is running on port ${HTTP_PORT}`);
 });
 
-// HTTPS server
-const privateKey = fs.readFileSync('private.key', 'utf8');
-const certificate = fs.readFileSync('certificate.crt', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-const httpsServer = https.createServer(credentials, app);
-const HTTPS_PORT = 443;
-
-httpsServer.listen(HTTPS_PORT, () => {
-    console.log(`HTTPS Server is running on port ${HTTPS_PORT}`);
-});
-
-
 
 
 
